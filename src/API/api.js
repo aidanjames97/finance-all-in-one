3ebac3b0fa2b8ad3a112a11ef66ff7a3bf96ec1e
Aidan James <ajame23@uwo.ca>
2024-08-06 15:44:49 -0400

    finished

--- src/API/api.js
+++ src/API/api.js
@@ -1,3 +1,7 @@
-
+const key = "cqajf41r01qjmbik6lfgcqajf41r01qjmbik6lg0";
+const apiKey = "AIzaSyB-9KcEy0wIxmG1Y4auk73Aq36MG5XcqnQ";
+const appId = "1:630875164458:web:e127918d2bacd7050e5853";
+const messagingSenderId = "630875164458";
+const measurementId = "G-P6KSZ0XBC5";
 
 export { apiKey, appId, messagingSenderId, measurementId, key };
